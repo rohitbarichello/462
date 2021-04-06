9bf818db07a6ff77c7a5a3937e56746719bc7414
Rohit Barichello <rohitbarichello@gmail.com>
2021-04-06 12:34:23 -0500

    lab 4 folder created

--- project/test.py
+++ project/test.py
@@ -1,8 +1,16 @@
-from picamera import PiCamera
-from time import sleep
+from cv2 import cv2
+import time
 
-camera = PiCamera()
+capture = cv2.VideoCapture(0)
 
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
+time.sleep(0.1)
+
+while True:
+    ret, frame = capture.read()
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(20) & 0xFF == ord('d'):
+        break
+
+
+cv2.destroyAllWindows()
